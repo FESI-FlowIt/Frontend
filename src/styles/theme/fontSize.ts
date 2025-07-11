5f8027d219cc5dade49fa9ad5d90290669ee62ee
00kang <kdb070970@gmail.com>
2025-07-11 14:11:32 +0900

    chore:#16 - initialize design system setup

---
+++ src/styles/theme/fontSize.ts
@@ -0,0 +1,21 @@
+export const fontSize = {
+  // Title
+  'display-32': ['32px', { fontWeight: '700', lineHeight: '40px' }],
+  'display-24': ['24px', { fontWeight: '700', lineHeight: '30px' }],
+  'head-20': ['20px', { fontWeight: '600', lineHeight: '26px' }],
+  'head-16': ['16px', { fontWeight: '600', lineHeight: '28px' }],
+  'head-long-16': ['16px', { fontWeight: '600', lineHeight: '30px' }],
+
+  // Body
+  'body-sb-20': ['20px', { fontWeight: '700', lineHeight: '22px' }],
+  'body-m-20': ['20px', { fontWeight: '700', lineHeight: '22px' }],
+  'body-20': ['20px', { fontWeight: '700', lineHeight: '22px' }],
+  'body-b-16': ['16px', { fontWeight: '700', lineHeight: '22px' }],
+  'body-m-16': ['16px', { fontWeight: '500', lineHeight: '24px' }],
+  'body-16': ['16px', { fontWeight: '400', lineHeight: '24px' }],
+  'body-long-m-16': ['16px', { fontWeight: '500', lineHeight: '34px' }],
+  'body-long-16': ['16px', { fontWeight: '400', lineHeight: '28px' }],
+  'body-b-12': ['12px', { fontWeight: '700', lineHeight: '22px' }],
+  'body-m-12': ['12px', { fontWeight: '500', lineHeight: '22px' }],
+  'body-12': ['12px', { fontWeight: '400', lineHeight: '22px' }],
+};
