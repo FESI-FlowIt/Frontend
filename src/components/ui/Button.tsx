ce041657ff0012cca794b69335f165aa22bf1a73
unknown <d653@naver.com>
2025-07-11 15:38:45 +0900

    Style:#19 - fix buttonVariants

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -4,30 +4,34 @@ import { cva, VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
 
-const buttonVariants = cva('flex items-center justify-center cursor-pointer', {
+const buttonVariants = cva('flex cursor-pointer items-center justify-center', {
   variants: {
     variant: {
-      default: 'bg-white',
-      secondary: 'bg-white',
-      disable: 'bg-white',
+      default: 'bg-primary-01 hover:bg-primary-01-hover',
+      secondary: 'bg-secondary-01 hover:secondary-01-hover',
+      disable: 'bg-disable',
+    },
+    text: {
+      default: 'text-body-sb-20 sm:text-body-m-16',
+      secondary: 'text-body-sb-20 sm:text-body-16',
     },
     size: {
-      auth: 'h-10',
-      check: 'h-5',
-      modal: 'h-8',
+      auth: 'h-[62px] w-[600px] px-5 py-6 sm:h-11 sm:w-full',
+      check: 'h-[60px] w-[118px] px-5 py-6 sm:h-11 sm:w-20',
+      modal: 'h-12 w-[520px]',
     },
     rounded: {
       none: 'rounded-none',
-      default: 'roundee-xl',
+      default: 'rounded-xl',
     },
   },
   defaultVariants: {
     variant: 'default',
+    text: 'default',
     size: 'auth',
     rounded: 'default',
   },
 });
-
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {}
