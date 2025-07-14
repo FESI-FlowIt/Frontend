16382095185947b87c0a1a43ab545c0c84898285
unknown <d653@naver.com>
2025-07-14 14:35:34 +0900

    Feat:#30 - add navigaion logic at Logo comp

--- src/components/auth/Logo.tsx
+++ src/components/auth/Logo.tsx
@@ -1,19 +1,38 @@
+'use client';
+
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 import google from '../../../public/assets/images/googleLogo.svg';
 import kakao from '../../../public/assets/images/kakaoLogo.svg';
 import naver from '../../../public/assets/images/naverLogo.svg';
 
 export default function Logo() {
+  const router = useRouter();
+
+  //ToDo: 소셜로그인 api가 나오면 router 설정
+  const handleNavigation = (provider: 'google' | 'kakao' | 'naver') => {
+    router.push(`/${provider}`);
+  };
+
   return (
     <div className="flex gap-20">
-      <div className="relative sm:h-44 sm:w-44 md:h-52 md:w-52 lg:h-52 lg:w-52">
+      <div
+        onClick={() => handleNavigation('naver')}
+        className="relative sm:h-44 sm:w-44 md:h-52 md:w-52 lg:h-52 lg:w-52"
+      >
         <Image src={naver} alt="네이버 로고 이미지" fill />
       </div>
-      <div className="relative sm:h-44 sm:w-44 md:h-52 md:w-52 lg:h-52 lg:w-52">
+      <div
+        onClick={() => handleNavigation('kakao')}
+        className="relative sm:h-44 sm:w-44 md:h-52 md:w-52 lg:h-52 lg:w-52"
+      >
         <Image src={kakao} alt="카카오 로고 이미지" fill />
       </div>
-      <div className="relative sm:h-44 sm:w-44 md:h-52 md:w-52 lg:h-52 lg:w-52">
+      <div
+        onClick={() => handleNavigation('google')}
+        className="relative sm:h-44 sm:w-44 md:h-52 md:w-52 lg:h-52 lg:w-52"
+      >
         <Image src={google} alt="구글 로고 이미지" fill />
       </div>
     </div>
