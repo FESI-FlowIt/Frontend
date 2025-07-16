11579b44322800952ddffd69a6173c73092d92ce
unknown <d653@naver.com>
2025-07-16 16:01:03 +0900

    Fix:#11 - reset page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import LoginForm from '@/components/auth/LoginForm';
-
 export default function Home() {
-  return (
-    <div className="mt-200 ml-500">
-      <LoginForm />
-    </div>
-  );
+  return <div>안녕하세요.</div>;
 }
