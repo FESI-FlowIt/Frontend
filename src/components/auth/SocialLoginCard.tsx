a9329ec99ce23f732a16d7ba1743fd17816337ec
unknown <d653@naver.com>
2025-07-16 17:57:17 +0900

    Fix:#30 - change structure of SocialLoginCard

--- src/components/auth/SocialLoginCard.tsx
+++ src/components/auth/SocialLoginCard.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { clsx } from 'clsx';
 interface ProvidersInterface {
   name: string;
   icon: string;
@@ -21,8 +22,15 @@ export default function SocialLoginCard({ mode }: { mode: 'login' | 'signUp' }) 
     router.push(`/${provider}`);
   };
 
-  return mode === 'login' ? (
-    <div className="flex flex-col items-center gap-40 sm:gap-24 md:gap-40">
+  return (
+    <div
+      className={clsx(
+        'flex flex-col items-center',
+        mode === 'login' && 'gap-40 sm:gap-24 md:gap-40',
+        mode === 'signUp' && 'gap-24',
+      )}
+    >
+      {mode === 'signUp' && <span className="text-body-sb-20 text-text-04">간편 회원가입</span>}
       <div className="flex gap-20">
         {providers.map(
           ({ name, icon }) =>
@@ -42,38 +50,17 @@ export default function SocialLoginCard({ mode }: { mode: 'login' | 'signUp' }) 
             ),
         )}
       </div>
-      <span className="text-body-16 flex gap-4">
-        FlowIt이 처음이신가요?
-        <span
-          onClick={() => router.push('/auth/signUp')}
-          className="text-primary-01 cursor-pointer underline"
-        >
-          회원가입
+      {mode === 'login' && (
+        <span className="text-body-16 flex gap-4">
+          FlowIt이 처음이신가요?
+          <span
+            onClick={() => router.push('/auth/signUp')}
+            className="text-primary-01 cursor-pointer underline"
+          >
+            회원가입
+          </span>
         </span>
-      </span>
-    </div>
-  ) : (
-    <div className="flex flex-col items-center gap-24">
-      <span className="text-body-sb-20 text-text-04">간편 회원가입</span>
-      <div className="flex gap-20">
-        {providers.map(
-          ({ name, icon }) =>
-            icon && (
-              <div
-                key={name}
-                onClick={() => handleNavigation(name as 'naver' | 'kakao' | 'google')}
-                className="relative h-52 w-52 cursor-pointer sm:h-44 sm:w-44 md:h-52 md:w-52"
-              >
-                <Image
-                  src={icon}
-                  alt={`${name} 로고 이미지`}
-                  fill
-                  sizes="(max-width: 768px) 40px, (max-width: 1200px) 52px, 52px"
-                />
-              </div>
-            ),
-        )}
-      </div>
+      )}
     </div>
   );
 }
