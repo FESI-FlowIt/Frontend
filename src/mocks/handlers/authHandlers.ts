0d9c5d2f0fc6311ed1a3579b63e111ca782b5492
unknown <d653@naver.com>
2025-07-17 10:34:38 +0900

    Fix:#11 - apply comment about seperating handler response

--- src/mocks/handlers/authHandlers.ts
+++ src/mocks/handlers/authHandlers.ts
@@ -1,18 +1,13 @@
 import { http, HttpResponse } from 'msw';
 
+import { loginErr, loginRes } from '../mockResponses/auth/loginResponse';
+
 export const authHandlers = [
   http.post('/auth/signIn', async ({ request }) => {
     const { email, password } = (await request.json()) as { email: string; password: string };
     if (email === 'test1@test.com' && password === 'asd123123') {
-      return HttpResponse.json({
-        id: '1',
-        email,
-        token: 'fake-jwt-token',
-      });
+      return HttpResponse.json(loginRes);
     }
-    return HttpResponse.json(
-      { message: '이메일 또는 비밀번호가 올바르지 않습니다.' },
-      { status: 401 },
-    );
+    return HttpResponse.json(loginErr, { status: 401 });
   }),
 ];
