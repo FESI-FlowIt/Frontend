f325499b7f1adbedeae55bae2ef1c883374f1b48
unknown <d653@naver.com>
2025-07-20 15:17:29 +0900

    Style:#48 - responsive design for SidebarHeader comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import SidebarHeader from '@/components/sidebar/SidebarHeader';
-
 export default function Home() {
-  return (
-    <div className="mt-100 ml-200">
-      <SidebarHeader />
-    </div>
-  );
+  return <div>안녕하세요.</div>;
 }
--- src/components/sidebar/SidebarHeader.tsx
+++ src/components/sidebar/SidebarHeader.tsx
@@ -10,18 +10,18 @@ export default function SidebarHeader() {
     console.log('사이드바 닫기');
   };
   return (
-    <div className="flex items-center gap-86">
-      <div className="flex items-center gap-6">
-        <div className="relative h-36 w-36">
+    <div className="flex items-center gap-86 sm:gap-117 md:gap-86">
+      <div className="sm:gap-4.6 flex items-center gap-6 md:gap-6">
+        <div className="relative h-36 w-36 sm:h-28 sm:w-28 md:h-36 md:w-36">
           <Image src="/assets/images/logoIcon.svg" alt="로고 아이콘" fill />
         </div>
-        <span className="text-logo-31 text-black">FlowIt</span>
+        <span className="text-logo-31 md:text-logo-31 sm:text-logo-24 text-black">FlowIt</span>
       </div>
       <button
         onClick={handleClick}
-        className="rounded-12 border-line hover:bg-sidebar-hover flex h-44 w-44 cursor-pointer items-center justify-center border bg-white hover:border-none"
+        className="rounded-12 border-line hover:bg-sidebar-hover flex h-44 w-44 cursor-pointer items-center justify-center border bg-white hover:border-none sm:h-32 sm:w-32 md:h-44 md:w-44"
       >
-        <SidebarHide className="h-24 w-24" />
+        <SidebarHide className="sm:h-17.45 sm:w-17.45 h-24 w-24 md:h-24 md:w-24" />
       </button>
     </div>
   );
