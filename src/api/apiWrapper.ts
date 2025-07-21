50a30d706951d2edca97f42abe692133a9c1c4c3
unknown <d653@naver.com>
2025-07-21 18:53:40 +0900

    Feat:#75 - add apiWrapper & authStore

---
+++ src/api/apiWrapper.ts
@@ -0,0 +1,84 @@
+import { RequestInit } from 'next/dist/server/web/spec-extension/request';
+
+import { useAuthStore } from '@/store/authStore';
+
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export class CustomError extends Error {
+  data: any;
+  constructor(message: string, data: any) {
+    super(message);
+    this.data = data;
+  }
+}
+
+export async function fetchWrapper(url: string, options: RequestInit = {}) {
+  const accessToken = useAuthStore.getState().accessToken;
+  const refreshToken = useAuthStore.getState().refreshToken;
+
+  const headers: any = {
+    'Cache-Control': 'no-cache',
+    ...options.headers,
+  };
+
+  if (accessToken) {
+    headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  try {
+    let response = await fetch(`${BASE_URL}${url}`, {
+      ...options,
+      headers,
+    });
+
+    if (!response.ok) {
+      if (response.status === 401 && refreshToken) {
+        const refreshResponse = await fetch(`${BASE_URL}/auth/tokens`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${refreshToken}`,
+          },
+        });
+
+        if (!refreshResponse.ok) {
+          throw new CustomError('Failed to refresh token', await refreshResponse.json());
+        }
+
+        const { accessToken: newAccessToken, refreshToken: newRefreshToken } =
+          await refreshResponse.json();
+
+        // zustand에 새로운 토큰 저장
+        useAuthStore.getState().setAccessToken(newAccessToken);
+        if (newRefreshToken) {
+          useAuthStore.getState().setRefreshToken(newRefreshToken);
+        }
+
+        // 새 accessToken으로 재시도
+        headers.Authorization = `Bearer ${newAccessToken}`;
+
+        response = await fetch(`${BASE_URL}${url}`, {
+          ...options,
+          headers,
+        });
+
+        if (!response.ok) {
+          throw new CustomError(`HTTP error! status: ${response.status}`, await response.json());
+        }
+      } else {
+        console.error('Response not OK:', {
+          status: response.status,
+          statusText: response.statusText,
+          url: response.url,
+        });
+        throw new CustomError(`HTTP error! status: ${response.status}`, await response.json());
+      }
+    }
+
+    if (response.status === 204) return;
+    return response.json();
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error;
+  }
+}
