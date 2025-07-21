3f1d133497d7dff0d540d0aa39421789f95d6feb
[CLOUD4] 임서현 <violethart123@gmail.com>
2025-07-21 15:45:16 +0900

    feat: timer ui

---
+++ src/stories/timer/Widget.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import TimerWidget from '@/components/timer/TimerWidget';
+
+const meta: Meta<typeof TimerWidget> = {
+  title: 'Components/timer/TimerWidget',
+  component: TimerWidget,
+  parameters: {
+    layout: 'centered',
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof TimerWidget>;
+
+export const Default: Story = {
+  render: () => <TimerWidget />,
+};
