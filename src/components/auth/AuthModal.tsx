0edf8faf42c42eb13c2868814cf7ddd90f3f6d3a
unknown <d653@naver.com>
2025-07-22 11:43:04 +0900

    Feat:#67 - connect auth modal to login & signup form

--- src/components/auth/AuthModal.tsx
+++ src/components/auth/AuthModal.tsx
@@ -10,23 +10,25 @@ type AuthModalProps = {
 export default function AuthModal({ isOpen, closeModal, mode }: AuthModalProps) {
   return (
     <Modal size="auth" padding="auth" rounded="auth" isOpen={isOpen} onClose={closeModal}>
-      <div className="flex flex-col items-center">
-        <span className="text-body-sb-20 text-black">
+      <div className="flex flex-col items-center py-30">
+        <span className="text-body-sb-20 mb-16 text-black">
           {mode === 'login' ? '로그인에 실패했어요 :(' : '회원가입에 실패했어요 :('}
         </span>
-        {mode === 'login' ? (
-          <span className="text-body-m-16 mb-16 mb-24 text-black">
-            아이디와 비밀번호를
-            <br />
-            다시 한번 확인해주세요
-          </span>
-        ) : (
-          <span className="text-body-m-16 mb-24 text-black">
-            입력 내용 확인 후<br />
-            다시 한번 확인해주세요
-          </span>
-        )}
-        <Button size="authModal" disabled={false}>
+        <div className="text-body-m-16 mb-24 flex flex-col items-center text-black">
+          {mode === 'login' ? (
+            <>
+              <span>아이디와 비밀번호를</span>
+              <span>다시 한번 확인해주세요</span>
+            </>
+          ) : (
+            <>
+              <span>입력 내용 확인 후</span>
+              <span>다시 한번 확인해주세요</span>
+            </>
+          )}
+        </div>
+
+        <Button onClick={closeModal} size="authModal" disabled={false}>
           확인
         </Button>
       </div>
