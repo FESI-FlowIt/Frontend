3018eb9be318fb6da4542f9cf5ee2cb679eb8270
[CLOUD4] 임서현 <violethart123@gmail.com>
2025-07-23 17:21:33 +0900

    chore: delete timerresponse file

--- src/stories/timer/Widget.stories.tsx
+++ src/stories/timer/Widget.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/nextjs';
 
 import TimerWidget from '@/components/timer/TimerWidget';
-import { goalSummariesRes } from '@/mocks/mockResponses/goals/goalsResponse'; // ✅ 경로 정확히 확인
+import { goalSummariesRes } from '@/mocks/mockResponses/goals/goalsResponse';
 
 const meta: Meta<typeof TimerWidget> = {
   title: 'Components/timer/TimerWidget',
@@ -16,5 +16,5 @@ export default meta;
 type Story = StoryObj<typeof TimerWidget>;
 
 export const Default: Story = {
-  render: () => <TimerWidget goals={goalSummariesRes} />, // ✅ mockGoals 전달
+  render: () => <TimerWidget goals={goalSummariesRes} />,
 };
