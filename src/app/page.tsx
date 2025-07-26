eb491391938df935375d771da1ca7ede94c23a1c
unknown <d653@naver.com>
2025-07-26 20:16:24 +0900

    Fix:#125 - initial page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
+import LandingBanner from '@/components/landing/LandingBanner';
+import LandingFeaturesDetail from '@/components/landing/LandingFeaturesDetail';
+import LandingIntroduction from '@/components/landing/LandingIntroduction';
+
 export default function Home() {
-  return <div>안녕하세요.</div>;
+  return (
+    <div>
+      <section className="mb-200">
+        <LandingBanner />
+      </section>
+
+      <section className="mb-240">
+        <LandingIntroduction />
+      </section>
+
+      <section className="mb-300">
+        <LandingFeaturesDetail />
+      </section>
+    </div>
+  );
 }
