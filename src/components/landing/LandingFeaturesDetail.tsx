3b1acc70ccdd6d8f1327324b4b3654dcc84433c7
unknown <d653@naver.com>
2025-07-26 20:05:21 +0900

    Style:#124 - landing features detail comp

--- src/components/landing/LandingFeaturesDetail.tsx
+++ src/components/landing/LandingFeaturesDetail.tsx
@@ -5,16 +5,19 @@ import { cn } from '@/lib/utils';
 
 export default function LandingFeaturesDetail() {
   return (
-    <div>
+    <div className="flex flex-col items-center gap-240">
       {LANDINGPAGEFEATUREDETAILS.map((detail, index) => {
         const isOdd = index % 2 === 1;
 
         return (
-          <div key={detail.name} className={cn('flex', isOdd ? 'flex-row-reverse' : 'flex-row')}>
-            <Image src={detail.imgUrl} alt={detail.name} width={900} height={700} />
-            <div>
-              <span>{detail.name}</span>
-              <div>
+          <div
+            key={detail.name}
+            className={cn('flex gap-120', isOdd ? 'ml-124 flex-row-reverse' : 'flex-row')}
+          >
+            <Image src={detail.imgUrl} alt={detail.name} width={700} height={700} />
+            <div className="mt-80 flex flex-col gap-40">
+              <span className="text-banner-44-bold text-text-01">{detail.name}</span>
+              <div className="text-text-03 text-display-24 flex flex-col gap-24">
                 {detail.contents.map(content => (
                   <span key={content.name}>{content.name}</span>
                 ))}
