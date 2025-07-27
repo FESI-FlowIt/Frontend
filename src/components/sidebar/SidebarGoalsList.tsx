9ff1cf61d8fc12e5a80102655b4e03a5f235a3ea
unknown <d653@naver.com>
2025-07-27 22:37:38 +0900

    Feat:#131 - add sidebar goal color mapping

--- src/components/sidebar/SidebarGoalsList.tsx
+++ src/components/sidebar/SidebarGoalsList.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/navigation';
 
 import { useSidebarGoalPinned, useSidebarGoals } from '@/hooks/useSidebar';
 import { SidebarGoals } from '@/interfaces/sidebar';
+import { colorHexToVar } from '@/lib/goalColorUtils';
 import { ROUTES } from '@/lib/routes';
 import { useUserStore } from '@/store/userStore';
 
@@ -27,32 +28,38 @@ export default function SidebarGoalsList() {
 
   return (
     <div className="flex flex-col gap-12 sm:gap-8 md:gap-12">
-      {data?.map((goal: SidebarGoals) => (
-        <div
-          key={goal.goalId}
-          className="flex h-52 w-260 items-center justify-between px-10 sm:h-40 sm:w-248 md:h-52 md:w-260"
-        >
+      {data?.map((goal: SidebarGoals) => {
+        const goalColor = colorHexToVar[goal.color.toLowerCase()];
+
+        return (
           <div
-            onClick={() => router.push(ROUTES.GOALS.DETAIL(goal.goalId))}
-            className="flex cursor-pointer items-center gap-20"
+            key={goal.goalId}
+            className="flex h-52 w-260 items-center justify-between px-10 sm:h-40 sm:w-248 md:h-52 md:w-260"
           >
-            <div className={`h-12 w-12 rounded-full bg-[${goal.color}]`} />
-            <span className="text-text-02 text-body-m-20 md:text-body-sb-20 sm:text-body-b-16 w-170 overflow-hidden overflow-ellipsis whitespace-nowrap">
-              {goal.name}
-            </span>
+            <div
+              onClick={() => router.push(ROUTES.GOALS.DETAIL(goal.goalId))}
+              className="flex cursor-pointer items-center gap-20"
+            >
+              <div className={`h-12 w-12 rounded-full ${goalColor}`} />
+              <span className="text-text-02 text-body-m-20 md:text-body-sb-20 sm:text-body-b-16 w-170 overflow-hidden overflow-ellipsis whitespace-nowrap">
+                {goal.name}
+              </span>
+            </div>
+            <button
+              onClick={() => handlePinClick({ goalId: goal.goalId, currentPinned: goal.isPinned })}
+              className="relative h-24 w-24 cursor-pointer sm:h-20 sm:w-20 md:h-24 md:w-24"
+            >
+              <Image
+                src={
+                  goal.isPinned ? '/assets/icons/pinIcon_on.svg' : '/assets/icons/pinIcon_off.svg'
+                }
+                alt="핀 오프 아이콘"
+                fill
+              />
+            </button>
           </div>
-          <button
-            onClick={() => handlePinClick({ goalId: goal.goalId, currentPinned: goal.isPinned })}
-            className="relative h-24 w-24 cursor-pointer sm:h-20 sm:w-20 md:h-24 md:w-24"
-          >
-            <Image
-              src={goal.isPinned ? '/assets/icons/pinIcon_on.svg' : '/assets/icons/pinIcon_off.svg'}
-              alt="핀 오프 아이콘"
-              fill
-            />
-          </button>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 }
