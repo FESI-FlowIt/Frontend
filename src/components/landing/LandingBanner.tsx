a5fdf42b45fdd738197fef04e64853d4ba7f6bdf
unknown <d653@naver.com>
2025-07-28 10:53:54 +0900

    Style:149 - add animation for landing banner comp

--- src/components/landing/LandingBanner.tsx
+++ src/components/landing/LandingBanner.tsx
@@ -1,18 +1,56 @@
 'use client';
 
+import { useEffect, useRef, useState } from 'react';
+
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
 import { ROUTES } from '@/lib/routes';
+import { cn } from '@/lib/utils';
 
 import { Button } from '../ui/Button';
 
 export default function LandingBanner() {
   const router = useRouter();
+  const btnRef = useRef<HTMLDivElement>(null);
+  const imgRef = useRef<HTMLDivElement>(null);
+  const [btnInView, setBtnInView] = useState(false);
+  const [imgInView, setImgInView] = useState(false);
+
+  useEffect(() => {
+    const createObserver = (
+      ref: React.RefObject<HTMLElement | null>,
+      setInView: React.Dispatch<React.SetStateAction<boolean>>,
+    ) => {
+      return new IntersectionObserver(
+        ([entry]) => {
+          setInView(entry.isIntersecting);
+        },
+        { threshold: 0.3 },
+      );
+    };
+
+    const leftObserver = createObserver(btnRef, setBtnInView);
+    const rightObserver = createObserver(imgRef, setImgInView);
+
+    if (btnRef.current) leftObserver.observe(btnRef.current);
+    if (imgRef.current) rightObserver.observe(imgRef.current);
+
+    return () => {
+      leftObserver.disconnect();
+      rightObserver.disconnect();
+    };
+  }, []);
 
   return (
     <div className="bg-primary-01 flex h-620 items-center justify-center gap-189">
-      <div className="flex flex-col gap-42">
+      <div
+        ref={btnRef}
+        className={cn('flex transform flex-col gap-42 transition-all duration-1000 ease-out', {
+          'translate-x-0 opacity-100': btnInView,
+          '-translate-x-50 opacity-0': !btnInView,
+        })}
+      >
         <div className="bg-text-01 p-6.67 rounded-12 flex h-80 w-80 items-center justify-center">
           <Image src="/assets/images/logoIcon.svg" alt="로고 아이콘" width={66.67} height={66.67} />
         </div>
@@ -42,7 +80,16 @@ export default function LandingBanner() {
         </div>
       </div>
 
-      <div className="relative flex items-center justify-center">
+      <div
+        ref={imgRef}
+        className={cn(
+          'relative flex transform items-center justify-center transition-all duration-1000 ease-out',
+          {
+            'translate-x-0 opacity-100': imgInView,
+            'translate-x-50 opacity-0': !imgInView,
+          },
+        )}
+      >
         <div className="rounded-tl-50 rounded-bl-50 bg-landing-blue absolute h-343 w-850 -translate-x-[2%]" />
         <Image
           src="/assets/images/dashboard.svg"
