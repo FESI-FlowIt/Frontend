1091d047ecee1c971a91dcac4c1a5a95c5ed816a
unknown <d653@naver.com>
2025-07-30 14:08:12 +0900

    Feat:#181 - add auth mapper func

--- src/hooks/auth/useUser.ts
+++ src/hooks/auth/useUser.ts
@@ -1,17 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { getUser } from '@/api/authApi';
-import { User, UserResponse } from '@/interfaces/auth';
+import { mapToUser } from '@/api/mapper/authMapper';
 
 export const useUser = ({ enabled = true }) => {
   return useQuery({
     queryKey: ['user'],
     queryFn: getUser,
     enabled,
-    select: (data: UserResponse): User => ({
-      id: data.result.id,
-      email: data.result.email,
-      name: data.result.name,
-    }),
+    select: mapToUser,
   });
 };
