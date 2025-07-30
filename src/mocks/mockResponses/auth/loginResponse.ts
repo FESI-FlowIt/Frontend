90b507811e9d2422fe4b192b768c6b903dce30e4
unknown <d653@naver.com>
2025-07-30 13:45:25 +0900

    refactor/#181 - fix auth handler

--- src/mocks/mockResponses/auth/loginResponse.ts
+++ src/mocks/mockResponses/auth/loginResponse.ts
@@ -3,3 +3,10 @@ export const loginRes = {
   email: 'test1@test.com',
   token: 'fake-jwt-token',
 };
+
+export const user = {
+  email: 'test1@test.com',
+  name: '김코드',
+  password: 'asd123123',
+  id: '1',
+};
