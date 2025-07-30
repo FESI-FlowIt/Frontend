35545e28a881ef69fedd980f6122b2f55310c1fc
unknown <d653@naver.com>
2025-07-30 11:28:47 +0900

    Style:80 - responsive design for landing banner comp

--- src/components/landing/LandingBanner.tsx
+++ src/components/landing/LandingBanner.tsx
@@ -43,29 +43,36 @@ export default function LandingBanner() {
   }, []);
 
   return (
-    <div className="bg-primary-01 flex h-620 items-center justify-center gap-189">
+    <div className="bg-primary-01 flex h-620 items-center sm:flex-col sm:justify-center sm:gap-30 md:flex-row md:justify-between md:gap-30 md:pl-32 lg:justify-center lg:gap-140">
       <div
         ref={btnRef}
-        className={cn('flex transform flex-col gap-42 transition-all duration-1000 ease-out', {
-          'translate-x-0 opacity-100': btnInView,
-          '-translate-x-10 opacity-0': !btnInView,
-        })}
+        className={cn(
+          'flex transform flex-col transition-all duration-1000 ease-out sm:gap-24 md:gap-32 lg:gap-42',
+          {
+            'translate-x-0 opacity-100': btnInView,
+            '-translate-x-10 opacity-0': !btnInView,
+          },
+        )}
       >
-        <div className="bg-text-01 p-6.67 rounded-12 flex h-80 w-80 items-center justify-center">
-          <Image src="/assets/images/logoIcon.svg" alt="로고 아이콘" width={66.67} height={66.67} />
+        <div className="bg-text-01 p-6.67 rounded-12 flex items-center justify-center sm:h-40 sm:w-40 md:h-40 md:w-40 lg:h-80 lg:w-80">
+          <div className="relative sm:h-33 sm:w-33 md:h-33 md:w-33 lg:h-67 lg:w-67">
+            <Image src="/assets/images/logoIcon.svg" alt="로고 아이콘" fill />
+          </div>
         </div>
-        <div className="mb-10">
-          <span className="text-banner-44 text-white">
+        <div className="sm:mb-0 md:mb-0 lg:mb-10">
+          <span className="lg:text-banner-44 md:text-banner-24 sm:text-banner-24 flex text-white sm:flex-col md:flex-col lg:block">
             목표 중심의
-            <br /> 스마트한 생산성 관리, <span className="text-banner-44-bold">FlowIt</span>
+            <br /> 스마트한 생산성 관리,{' '}
+            <span className="lg:text-banner-44-bold md:text-logo-32 sm:text-logo-32">FlowIt</span>
           </span>
         </div>
-        <div className="flex gap-20">
+        <div className="flex sm:gap-12 md:gap-12 lg:gap-20">
           <Button
             variant="secondary"
             text="secondary"
             disabled={false}
             onClick={() => router.push(ROUTES.AUTH.LOGIN)}
+            className="sm:h-44 sm:w-118 md:h-44 md:w-118 lg:h-62 lg:w-240"
           >
             로그인
           </Button>
@@ -74,6 +81,7 @@ export default function LandingBanner() {
             text="secondary"
             disabled={false}
             onClick={() => router.push(ROUTES.AUTH.SIGNUP)}
+            className="sm:h-44 sm:w-118 md:h-44 md:w-118 lg:h-62 lg:w-240"
           >
             회원가입
           </Button>
@@ -90,14 +98,10 @@ export default function LandingBanner() {
           },
         )}
       >
-        <div className="rounded-tl-50 rounded-bl-50 bg-landing-blue absolute h-343 w-850 -translate-x-[2%]" />
-        <Image
-          src="/assets/images/dashboard.svg"
-          alt="대쉬보드 이미지"
-          width={699.78}
-          height={399.88}
-          className="relative z-10"
-        />
+        <div className="rounded-tl-50 rounded-bl-50 bg-landing-blue absolute sm:hidden md:block md:h-240 md:w-400 md:-translate-x-[5%] lg:block lg:h-343 lg:w-1045 lg:translate-x-[8%]" />
+        <div className="relative z-10 sm:h-194 sm:w-340 md:h-206 md:w-361 lg:h-400 lg:w-700">
+          <Image src="/assets/images/dashboard.svg" alt="대쉬보드 이미지" fill />
+        </div>
       </div>
     </div>
   );
