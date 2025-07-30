06de205c58d083697cb47089eaeaaa02d22dc783
unknown <d653@naver.com>
2025-07-30 16:28:06 +0900

    Feat:#188 - add og image & tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,10 +14,27 @@ const pretendard = localFont({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('http://3.35.108.14/'),
   title: 'FlowIt',
   description: '작업 패턴 분석을 통해 개인의 생산성을 극대화하는 서비스입니다.',
   icons: {
     icon: '/favicon.ico',
+  },
+  openGraph: {
+    title: 'FlowIt',
+    description: 'FlowIt은 작업 패턴 분석을 통해 개인의 생산성을 극대화하는 서비스',
+    url: 'http://3.35.108.14/',
+    images: [
+      {
+        url: 'opengraph-image.png',
+        alt: '서비스 설명 이미지',
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'ko_KR',
+    type: 'website',
+    siteName: 'FlowIt',
   },
 };
 
