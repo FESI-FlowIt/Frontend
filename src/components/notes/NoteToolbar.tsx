a1c027805c31ea13508b308107924c1ba8592f7e
epass1123 <404.notfound@kakao.com>
2025-07-30 13:47:45 +0900

    feat: move toolbarGroups to a separate constants file

--- src/components/notes/NoteToolbar.tsx
+++ src/components/notes/NoteToolbar.tsx
@@ -1,22 +1,5 @@
 import { IconButton } from '@/components/ui/IconButton';
-
-const toolbarGroups = [
-  [
-    ['bold', '굵게'],
-    ['italic', '기울임꼴'],
-    ['underline', '밑줄'],
-  ],
-  [
-    ['alignmentleft', '왼쪽 정렬'],
-    ['alignmentcenter', '가운데 정렬'],
-    ['alignmentright', '오른쪽 정렬'],
-  ],
-  [
-    ['bullet', '글머리 기호'],
-    ['numbering', '번호 매기기'],
-    ['coloring', '색상'],
-  ],
-] as const;
+import { toolbarGroups } from '@/constants/toolBarGroups';
 
 const linkGroup = [['link', '링크첨부']] as const;
 
