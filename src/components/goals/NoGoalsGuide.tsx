1cee53b4dd82ef8f9bbe64ea548b1bdae7632e00
[CLOUD4] 임서현 <violethart123@gmail.com>
2025-07-30 17:43:32 +0900

    chore: Sort the import order

--- src/components/goals/NoGoalsGuide.tsx
+++ src/components/goals/NoGoalsGuide.tsx
@@ -1,8 +1,8 @@
 'use client';
 
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 
-import Image from 'next/image';
 import { Button } from '@/components/ui/Button';
 
 export default function NoGoalsGuide() {
