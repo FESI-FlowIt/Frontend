a3320f3a504cf6215c75235a4189b85188db4f55
[CLOUD4] 임서현 <violethart123@gmail.com>
2025-08-01 13:41:19 +0900

    refactor: card size

--- src/components/goals/NoGoalsGuide.tsx
+++ src/components/goals/NoGoalsGuide.tsx
@@ -1,37 +1,43 @@
 'use client';
 
 import Image from 'next/image';
-import { useRouter } from 'next/navigation';
+import { useModalStore } from '@/store/modalStore';
 
 import { Button } from '@/components/ui/Button';
+import GoalModal from '../goals/GoalModal';
 
 export default function NoGoalsGuide() {
-  const router = useRouter();
+  const { openGoalModal } = useModalStore();
 
   return (
     <div className="flex flex-col items-center">
-      <Image
-        src="/assets/images/landing_img.svg"
-        alt="랜딩페이지 이미지"
-        width={140}
-        height={140}
-        className="mb-16 md:h-200 md:w-200"
-      />
+      <div className="relative mb-16 h-140 w-140 md:h-200 md:w-200">
+        <Image
+          src="/assets/images/landing_img.svg"
+          alt="랜딩페이지 이미지"
+          fill
+          className="object-contain"
+        />
+      </div>
+
       <p className="text-text-04 text-body-m-20 mb-20 text-center">
         목표를 만들어
         <br />
         세부 할 일을 추가해봐요
       </p>
+
       <Button
         type="button"
         size="addgoal"
         variant="default"
         text="default"
         disabled={false}
-        onClick={() => router.push('/goals/create')}
+        onClick={openGoalModal}
       >
         + 목표 만들기
       </Button>
+
+      <GoalModal />
     </div>
   );
 }
