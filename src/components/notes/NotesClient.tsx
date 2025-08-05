fd3b9fda8cea21deca41423fc1a9ec4b033410da
김기정 <59950506+epass1123@users.noreply.github.com>
2025-08-05 17:05:46 +0900

    feat: add note list ui components

---
+++ src/components/notes/NotesClient.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { useState } from 'react';
+
+import { MOCK_TODOS_WITH_NOTES } from '@/components/notes/constants';
+import TodoWithNoteList from '@/components/notes/TodoWithNoteList';
+import GoalSelector from '@/components/todos/GoalSelector';
+
+const NotesClient = () => {
+  const [selectedGoalId, setSelectedGoalId] = useState<number>(0);
+
+  return (
+    <div className="mx-auto h-full max-w-1184">
+      <header className="mb-44">
+        <div className="flex items-center">
+          <h1 className="text-text-01 text-display-32 font-bold">노트 모아보기</h1>
+        </div>
+      </header>
+
+      <div className="flex">
+        <main className="flex-1">
+          <div className="mb-32">
+            <GoalSelector
+              selectedGoalId={selectedGoalId}
+              onSelectGoal={setSelectedGoalId}
+              variant="notes"
+            />
+          </div>
+          <TodoWithNoteList todos={MOCK_TODOS_WITH_NOTES} />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default NotesClient;
---
+++ src/components/notes/TodoWithNoteItem.tsx
@@ -0,0 +1,29 @@
+import Todo from '@/assets/icons/todo.svg';
+import { TodoWithNotes } from '@/interfaces/todo';
+
+interface TodoWithNoteItemProps {
+  todo: TodoWithNotes;
+}
+
+const TodoWithNoteItem = ({ todo }: TodoWithNoteItemProps) => {
+  return (
+    <div className="border-line rounded-12 h-full w-full border-1 bg-white p-20">
+      <div className="flex items-center justify-between">
+        <div className="flex-row items-center">
+          <div className="mb-8 flex gap-8">
+            <div className="flex h-20 w-20 items-center justify-center">
+              <Todo className="text-gray-01 h-full w-full" />
+            </div>
+            <div className="text-text-02 text-body-b-16">{todo.isDone ? 'Done' : 'To do'}</div>
+          </div>
+          <div className="text-body-m-20 text-text-02">{todo.title}</div>
+        </div>
+        <button className="text-primary-01 text-body-b-16 bg-heatmap-1 rounded-[5px] p-8 font-medium">
+          노트 3개
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default TodoWithNoteItem;
