48344a35ff3b077b81c3959d940c0125189580e9
unknown <d653@naver.com>
2025-08-05 13:38:37 +0900

    Fix:#221 - not save id of user at zustand

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -4,7 +4,7 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 import type { User } from '@/interfaces/auth';
 
 interface UserState {
-  user: User | null;
+  user: Omit<User, 'id'> | null;
   setUser: (_user: User | null) => void;
   clearUser: () => void;
 }
@@ -13,7 +13,14 @@ export const useUserStore = create<UserState>()(
   persist(
     (set): UserState => ({
       user: null,
-      setUser: newUser => set({ user: newUser }),
+      setUser: newUser => {
+        if (newUser) {
+          const rest = { ...newUser } as Omit<User, 'id'>;
+          set({ user: rest });
+        } else {
+          set({ user: null });
+        }
+      },
       clearUser: () => set({ user: null }),
     }),
     {
