d1cf28482a3839357b6a5bdc7e34b156737fdadc
unknown <d653@naver.com>
2025-08-08 01:21:20 +0900

    Fix:#183 - update call back page at middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,12 +3,20 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function middleware(req: NextRequest) {
   const token = req.cookies.get('accessToken')?.value;
 
-  const publicPaths = ['/', '/auth/login', '/auth/signup'];
+  const publicPaths = ['/', '/auth/login', '/auth/signup', '/oauth/callback'];
   const pathname = req.nextUrl.pathname;
   const isPublicPath = publicPaths.includes(pathname);
   const isFile = pathname.match(/\.(.*)$/);
 
   if (isFile) {
+    return NextResponse.next();
+  }
+
+  if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/api') ||
+    pathname.startsWith('/oauth/callback')
+  ) {
     return NextResponse.next();
   }
 
