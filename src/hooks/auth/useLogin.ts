dd151ee0a6988a32a592baf521e64af486b68693
unknown <d653@naver.com>
2025-10-21 20:55:48 +0900

    Fix:#306 - change hook logic

--- src/hooks/auth/useLogin.ts
+++ src/hooks/auth/useLogin.ts
@@ -1,25 +1,37 @@
 import { useRouter } from 'next/navigation';
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { postLogin } from '@/api/authApi';
+import { getUser, postLogin } from '@/api/authApi';
 import { LoginFormData } from '@/components/auth/LoginForm';
 import { ROUTES } from '@/lib/routes';
 import { useAuthStore } from '@/store/authStore';
+import { useUserStore } from '@/store/userStore';
 
 type Options = {
   onError: () => void;
+  onMutate?: () => void;
 };
 
-export default function useLogin({ onError }: Options) {
+export default function useLogin({ onError, onMutate }: Options) {
   const { setAccessToken } = useAuthStore();
+  const { setUser } = useUserStore();
   const router = useRouter();
+  const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: ({ email, password }: LoginFormData) => postLogin(email, password),
-    onSuccess: data => {
+    onSuccess: async data => {
       setAccessToken(data.accessToken);
+      const user = await queryClient.fetchQuery({
+        queryKey: ['user'],
+        queryFn: getUser,
+      });
+
+      setUser(user.result);
+
       router.push(ROUTES.DASHBOARD);
     },
     onError,
+    onMutate,
   });
 }
