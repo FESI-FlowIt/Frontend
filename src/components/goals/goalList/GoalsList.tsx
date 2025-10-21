a6adf8c816338b558b42bb724543a11707089097
unknown <d653@naver.com>
2025-10-21 21:45:39 +0900

    Feat:#311 - add empty ui for not goals list

--- src/components/goals/goalList/GoalsList.tsx
+++ src/components/goals/goalList/GoalsList.tsx
@@ -3,6 +3,8 @@
 import { useSidebar } from '@/app/providers/SidebarProvider';
 import { GoalSummary } from '@/interfaces/goal';
 
+import NoGoalsGuide from '../NoGoalsGuide';
+
 import GoalCard from './GoalCard';
 
 interface GoalsListProps {
@@ -13,13 +15,21 @@ const GoalsList = ({ goals }: GoalsListProps) => {
   const { isOpen } = useSidebar();
 
   return (
-    <div
-      className={`grid sm:grid-cols-1 sm:gap-16 md:grid-cols-2 md:gap-12 lg:gap-24 xl:grid-cols-3 ${isOpen ? 'lg:w-866 lg:grid-cols-2 xl:w-full' : 'lg:grid-cols-3'}`}
-    >
-      {goals.map(goal => (
-        <GoalCard key={goal.goalId} goal={goal} />
-      ))}
-    </div>
+    <>
+      {goals.length > 0 ? (
+        <div
+          className={`grid sm:grid-cols-1 sm:gap-16 md:grid-cols-2 md:gap-12 lg:gap-24 xl:grid-cols-3 ${isOpen ? 'lg:w-866 lg:grid-cols-2 xl:w-full' : 'lg:grid-cols-3'}`}
+        >
+          {goals.map(goal => (
+            <GoalCard key={goal.goalId} goal={goal} />
+          ))}
+        </div>
+      ) : (
+        <div className="flex h-full w-full items-center justify-center">
+          <NoGoalsGuide />
+        </div>
+      )}
+    </>
   );
 };
 
