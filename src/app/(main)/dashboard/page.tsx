310c1b72c8eeba6edd027e16d214e4920f485aed
00kang <kdb070970@gmail.com>
2025-10-25 23:47:33 +0900

    Feat:#317 - center page content and add md rail offset

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -5,30 +5,32 @@ import GoalListDashboardSection from '@/components/goals/GoalListDashboardSectio
 import HeatmapSection from '@/components/heatmaps/HeatmapSection';
 import ScheduleSection from '@/components/schedule/ScheduleSection';
 import TimerWidget from '@/components/timer/TimerWidget';
-import { cn } from '@/lib/utils';
 
 export default function DashboardPage() {
   return (
-    <div className={cn('sm:mt-54 sm:ml-0 md:mt-0 md:ml-88 lg:mt-0 lg:ml-0')}>
-      {/* 모바일/태블릿: 세로 스택 */}
-      <div className="space-y-24 lg:hidden">
-        <HeatmapSection />
-        <CalendarSection />
-        <ScheduleSection />
-        <GoalListDashboardSection />
-      </div>
-
-      {/* PC: 복합 레이아웃 */}
-      <div className="mx-auto hidden max-w-1920 lg:flex lg:h-full lg:flex-col lg:gap-24">
-        <div className="flex flex-1 gap-24">
+    <div className="mt-54 md:mt-0">
+      <div className="mx-auto w-full max-w-1200 md:pl-80 lg:px-30">
+        {/* 모바일/태블릿: 세로 스택 */}
+        <div className="space-y-24 lg:hidden">
           <HeatmapSection />
-          <div className="flex flex-col gap-12">
-            <CalendarSection />
-            <ScheduleSection />
-          </div>
+          <CalendarSection />
+          <ScheduleSection />
+          <GoalListDashboardSection />
         </div>
 
-        <GoalListDashboardSection />
+        {/* PC: 복합 레이아웃 */}
+        <div className="hidden lg:flex lg:h-full lg:flex-col lg:gap-24 lg:pl-80">
+          <div className="flex flex-1 gap-24">
+            <HeatmapSection />
+
+            <div className="flex flex-col gap-12">
+              <CalendarSection />
+              <ScheduleSection />
+            </div>
+          </div>
+
+          <GoalListDashboardSection />
+        </div>
       </div>
 
       <TimerWidget />
