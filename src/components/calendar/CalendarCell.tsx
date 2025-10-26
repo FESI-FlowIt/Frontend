442418d41e67d0a6725228fd88608bab17a1d3ba
00kang <kdb070970@gmail.com>
2025-10-26 16:52:10 +0900

    Feat:#319 - add previous/next month dates to fill empty cells

--- src/components/calendar/CalendarCell.tsx
+++ src/components/calendar/CalendarCell.tsx
@@ -4,12 +4,21 @@ import { cn } from '@/lib/utils';
 
 interface CalendarCellProps {
   date: number;
+  isCurrentMonth?: boolean;
+  isToday?: boolean;
   goals?: Goal[];
   onClick?: (date: number, goals: Goal[], event: React.MouseEvent) => void;
   className?: string;
 }
 
-const CalendarCell = ({ date, goals = [], onClick, className }: CalendarCellProps) => {
+const CalendarCell = ({
+  date,
+  isCurrentMonth = true,
+  isToday = false,
+  goals = [],
+  onClick,
+  className,
+}: CalendarCellProps) => {
   const hasGoals = goals.length > 0;
   const firstGoal = goals[0];
   const goalColorName = firstGoal ? getGoalBackgroundColorClass(firstGoal.color) : null;
@@ -26,10 +35,19 @@ const CalendarCell = ({ date, goals = [], onClick, className }: CalendarCellProp
       className={cn(
         'rounded-4 flex h-40 w-full min-w-40 cursor-pointer flex-col items-center p-1 transition md:max-h-44 md:max-w-84 lg:h-40 lg:w-71',
         hasGoals && 'hover:bg-tertiary-01',
+        !isCurrentMonth && 'opacity-40',
         className,
       )}
     >
-      <span className="text-body-m-16 text-text-03 h-20 self-center">{date}</span>
+      <span
+        className={cn(
+          'text-body-m-16 h-20 self-center',
+          isCurrentMonth ? 'text-text-03' : 'text-text-04',
+          isToday && 'text-body-b-16',
+        )}
+      >
+        {date}
+      </span>
 
       {hasGoals && (
         <div
